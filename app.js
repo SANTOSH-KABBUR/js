d9303819f2e4349865fa090812c99bbede6ca26c
SANTOSH-KABBUR <76898680+SANTOSH-KABBUR@users.noreply.github.com>
2021-09-08 12:55:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,32 @@ const rock = document.querySelector('rock')
 const paper = document.querySelector('paper')
 const scissor = document.querySelector('scissor')
 
+const handler = (e)=>{
+    console.log("e.target.id")
+
+switch (action) {
+    case "rock":
+        board.innerHTML ="Rock crushes scissors"
+        break;
+    case "paper":
+        board.innerHTML ="Paper traps Rock"
+        break;
+    case "scissor":
+        board.innerHTML ="Scissor cuts paper"
+        break;
+    default:
+        board.innerHTML =Click Something"
+        break;
+}
+
+
+
+
+
+
+}
+rock.addEventListener("click",handler("rock"))
+paper.addEventListener("click",handler("paper"))
+scissor.addEventListener("click",handler("scissor"))
+
+
